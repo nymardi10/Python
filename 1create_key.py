5e57539984a1ecd4f524e5508e3e60c361a43c23
nymardi10 <nytechnician@gmail.com>
2020-09-12 12:44:18 -0700

    all

--- 1create_key.py
+++ 1create_key.py
@@ -10,6 +10,35 @@ MAX_AGE      = 0
 
 def main():
 
+ check_for_creation()
+ check_for_deactivation()
+ check_for_deletion()
+
+def days_old(create_date):
+        now = datetime.now(timezone.utc)
+        age = now - create_date
+        return age.days    
+
+def create_tags(uname, key_id):
+    client.untag_user(
+    UserName=uname,
+    TagKeys=[
+        "active_key_id",
+    ]
+)
+
+    client.tag_user(
+    UserName=uname,
+    Tags=[
+        {
+            'Key': "active_key_id",
+            'Value': key_id
+       },
+    ]
+
+)
+
+def check_for_creation():
     response = client.list_users()
     for user in response['Users']:
         username = user['UserName']
@@ -20,31 +49,93 @@ def main():
             age = days_old(create_date)
             if age >= MAX_AGE:
                 print("Creating " + username + " key and set it to Active")
-                create_tags(username, access_key_id)
-                create_access_key(username)   
+                create_tags(username, create_access_key(username)) 
                 send_email_report(username, username, MAX_AGE , username)
 
-def days_old(create_date):
-        now = datetime.now(timezone.utc)
-        age = now - create_date
-        return age.days    
+def check_for_deletion():
+    userpaginate = client.get_paginator('list_users')
 
-def create_tags(uname, key_id):
-    client.tag_user(
-    UserName=uname,
-    Tags=[
-        {
-            'Key': key_id,
-            'Value': key_id
-       },
-    ]
-)
+    for user in userpaginate.paginate():
+        for username in user['Users']:
+            active_key_count = 0
+            inactive_key_count = 0
+            tags = client.list_user_tags(UserName = username['UserName'])
+            acc_key_to_delete = client.list_access_keys(UserName = username['UserName'])
+            for key in acc_key_to_delete['AccessKeyMetadata']:
+                status = key['Status']
+                if status == 'Active':
+                    active_key_count += 1
+                else: 
+                    inactive_key_count += 1
+            print(status)
+            print(active_key_count)
+            if active_key_count >= 1 and inactive_key_count >= 1:
+                access_key_age = key['CreateDate']
+                if tags['Tags']:
+                    for tag in tags['Tags']:
+                        if tag['Key'] == 'active_key_id':
+                           print(tag['Value'])
+                           active_key_value = tag['Value']
+                delete_all = False
+                for key in acc_key_to_delete['AccessKeyMetadata']:
+                    if key['AccessKeyId'] == active_key_value:
+                        create_date = key['CreateDate']
+                        age = days_old(create_date)
+                        if age >= 0:
+                            delete_all = True
+                            break
+                if delete_all:  
+                    for key in acc_key_to_delete['AccessKeyMetadata']:
+                        if key['AccessKeyId'] != active_key_value and key['Status'] == 'Inactive':
+                            client.delete_access_key(
+                            UserName=username['UserName'],
+                            AccessKeyId=key['AccessKeyId']
+                            )
+
+def check_for_deactivation():
+    userpaginate = client.get_paginator('list_users')
+
+    for user in userpaginate.paginate():
+        for username in user['Users']:
+            key_count = 0
+            tags = client.list_user_tags(UserName = username['UserName'])
+            acc_key_to_delete = client.list_access_keys(UserName = username['UserName'])
+            for key in acc_key_to_delete['AccessKeyMetadata']:
+                status = key['Status']
+                if status == 'Active':
+                    key_count += 1
+            print(status)
+            print(key_count)
+            if key_count > 1:
+                #for key in acc_key_to_delete['AccessKeyMetadata']:
+                access_key_age = key['CreateDate']
+                if tags['Tags']:
+                    for tag in tags['Tags']:
+                        if tag['Key'] == 'active_key_id':
+                           print(tag['Value'])
+                           active_key_value = tag['Value']
+                deactivate_all = False
+                for key in acc_key_to_delete['AccessKeyMetadata']:
+                    if key['AccessKeyId'] == active_key_value:
+                        create_date = key['CreateDate']
+                        age = days_old(create_date)
+                        if age >= 0:
+                            deactivate_all = True
+                            break
+                if deactivate_all:  
+                    for key in acc_key_to_delete['AccessKeyMetadata']:
+                        if key['AccessKeyId'] != active_key_value:
+                            client.update_access_key(
+                            UserName=username['UserName'],
+                            AccessKeyId=key['AccessKeyId'],
+                            Status='Inactive')
          
 def create_access_key(uname):
     list_users = client.create_access_key(UserName=uname)
     key_id = list_users['AccessKey']['AccessKeyId']
     secret_key = list_users['AccessKey']['SecretAccessKey']
     create_secret(key_id, secret_key, uname) 
+    return key_id
 
 def create_secret(access_key, secret_key, uname):
     secret.update_secret(
